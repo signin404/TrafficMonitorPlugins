366fef547a2ac62a6b87dc192c6ee8a4b9dbd6c9
signin404 <145902543+signin404@users.noreply.github.com>
2025-06-21 23:30:30 +0800

    Update HardwareMonitorItem.h

--- Plugins/HardwareMonitor/HardwareMonitorItem.h
+++ Plugins/HardwareMonitor/HardwareMonitorItem.h
@@ -1,48 +1,35 @@
-﻿#pragma once
-#include "PluginInterface.h"
-#include <string>
-#include "CommonData.h"
+#pragma once
+
 // ==================== 新增此行，解决所有问题的关键 ====================
 #include "../../include/PluginInterface.h"
 // ====================================================================
 
-namespace HardwareMonitor
+class CHardwareMonitorItem :
+    public IPluginItem
 {
-    class CHardwareMonitorItem : public IPluginItem
-    {
-    public:
-        CHardwareMonitorItem(const std::wstring& _identifier, const std::wstring& _item_name, const std::wstring& _label_text);
-        CHardwareMonitorItem(const std::wstring& identifyer, const std::wstring& item_name, const std::wstring& lable_text, bool is_graph, GraphType graph_mode);
+public:
+    // 原有构造函数
+    CHardwareMonitorItem(const std::wstring& identifyer, const std::wstring& item_name, const std::wstring& lable_text);
+    // 新增的构造函数，用于支持图形显示
+    CHardwareMonitorItem(const std::wstring& identifyer, const std::wstring& item_name, const std::wstring& lable_text, bool is_graph, GraphType graph_mode);
+    // 析构函数
+    ~CHardwareMonitorItem();
 
-        ~CHardwareMonitorItem();
+    // 实现继承自 IPluginItem 的虚函数
     virtual const wchar_t* GetItemName() const override;
     virtual const wchar_t* GetItemLableText() const override;
     virtual const wchar_t* GetItemValueText() const override;
-    virtual const wchar_t* GetItemValueTip() const override;
     virtual int GetItemWidth() const override;
-        // 通过 IPluginItem 继承
-        const wchar_t* GetItemName() const override;
-        const wchar_t* GetItemId() const override;
-        const wchar_t* GetItemLableText() const override;
-        const wchar_t* GetItemValueText() const override;
-        const wchar_t* GetItemValueSampleText() const override;
-        virtual int IsDrawResourceUsageGraph() const override;
-        virtual float GetResourceUsageGraphValue() const override;
 
-        void UpdateValue();
-        const std::wstring& GetIdentifier() const;
+    // 这是此类特有的函数 (不在 IPluginItem 中)
+    const wchar_t* GetItemValueTip() const;
 
-    private:
-        std::wstring identifier;
-        std::wstring item_name;
-        std::wstring item_value_taskbar;
-        std::wstring item_value_main_wnd;
-        std::wstring label_text;
-        int sensor_type{};
-        float item_value_num{};
+    // 这也是此类特有的函数
+    void UpdateValue();
+
+private:
     std::wstring m_identifyer;
     std::wstring m_item_name;
     std::wstring m_lable_text;
     float m_value{};
-    };
-}
+};
