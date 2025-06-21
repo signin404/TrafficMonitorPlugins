d6fa796004547f2b2eba49917f5c5f2aad989f0c
signin404 <145902543+signin404@users.noreply.github.com>
2025-06-21 23:30:53 +0800

    Update HardwareMonitorItem.cpp

--- Plugins/HardwareMonitor/HardwareMonitorItem.cpp
+++ Plugins/HardwareMonitor/HardwareMonitorItem.cpp
@@ -1,15 +1,14 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include "HardwareMonitorItem.h"
-#include "HardwareMonitor.h"
 #include "HardwareMonitorHelper.h"
 #include "Common.h"
-
-using namespace LibreHardwareMonitor::Hardware;
+#include "HardwareMonitor.h"
 
 namespace HardwareMonitor
 {
-    CHardwareMonitorItem::CHardwareMonitorItem(const std::wstring& _identifier, const std::wstring& _item_name, const std::wstring& _label_text)
-        : identifier(_identifier), item_name(_item_name), label_text(_label_text)
+    // 原有构造函数实现
+    CHardwareMonitorItem::CHardwareMonitorItem(const std::wstring& identifyer, const std::wstring& item_name, const std::wstring& lable_text)
+        : m_identifyer(identifyer), m_item_name(item_name), m_lable_text(lable_text)
     {
     }
 
@@ -17,7 +16,7 @@ namespace HardwareMonitor
     CHardwareMonitorItem::CHardwareMonitorItem(const std::wstring& identifyer, const std::wstring& item_name, const std::wstring& lable_text, bool is_graph, GraphType graph_mode)
         : m_identifyer(identifyer), m_item_name(item_name), m_lable_text(lable_text)
     {
-        // 直接访问和设置从 IPluginItem 继承来的成员
+        // 在此设置从基类继承的成员
         show_graph = is_graph;
         graph_type = graph_mode;
     }
@@ -29,109 +28,62 @@ namespace HardwareMonitor
 
     const wchar_t* CHardwareMonitorItem::GetItemName() const
     {
-        return item_name.c_str();
-    }
-
-    const wchar_t* CHardwareMonitorItem::GetItemId() const
-    {
-        static std::wstring item_id;
-        item_id = L"7KrUeFVl_" + identifier;
-        return item_id.c_str();
+        return m_item_name.c_str();
     }
 
     const wchar_t* CHardwareMonitorItem::GetItemLableText() const
     {
-        return label_text.c_str();
+        return m_lable_text.c_str();
     }
 
     const wchar_t* CHardwareMonitorItem::GetItemValueText() const
     {
-        if (CHardwareMonitor::GetInstance()->m_tm_settings.is_taskbar)
-            return item_value_taskbar.c_str();
-        else
-            return item_value_main_wnd.c_str();
-    }
-
-    const wchar_t* CHardwareMonitorItem::GetItemValueSampleText() const
-    {
-        static std::wstring sample_text;
-        const ItemInfo& item_info{ CHardwareMonitor::GetInstance()->m_settings.FindItemInfo(identifier) };
-        if (item_info.specify_value_width)
+        static std::wstring str;
+        const auto& settings = CHardwareMonitor::GetInstance()->GetSettings().GetItemInfo(m_identifyer);
+        if (settings.specify_value_width)
         {
-            sample_text = std::wstring(item_info.value_width, L'0');
-            if (item_info.decimal_places > 0)
-            {
-                sample_text += L'.';
-                sample_text += std::wstring(item_info.decimal_places, L'0');
-            }
-            if (item_info.show_unit)
-            {
-                if (CHardwareMonitor::GetInstance()->m_tm_settings.sperate_with_space())
-                    sample_text += L' ';
-                SensorType type = static_cast<SensorType>(sensor_type);
-                std::wstring unit;
-                if (!item_info.unit.empty())
-                    unit = item_info.unit;
-                else
-                    unit = Common::StringToStdWstring(HardwareMonitorHelper::GetSensorTypeDefaultUnit(type));
-                sample_text += unit;
-            }
+            wchar_t format_str[16];
+            swprintf_s(format_str, L"%%%d.%df", settings.value_width, settings.decimal_places);
+            wchar_t buff[32];
+            swprintf_s(buff, format_str, m_value);
+            str = buff;
         }
         else
         {
-            std::wstring item_value(GetItemValueText());
-            if (item_value.empty())
-                sample_text = L"0.00";
-            else
-                sample_text = item_value;
+            wchar_t format_str[16];
+            swprintf_s(format_str, L"%%.%df", settings.decimal_places);
+            wchar_t buff[32];
+            swprintf_s(buff, format_str, m_value);
+            str = buff;
         }
-        return sample_text.c_str();
+        if (settings.show_unit)
+            str += settings.unit;
+        return str.c_str();
     }
 
-    int CHardwareMonitorItem::IsDrawResourceUsageGraph() const
+    const wchar_t* CHardwareMonitorItem::GetItemValueTip() const
     {
-        SensorType type = static_cast<SensorType>(sensor_type);
-        return type == SensorType::Temperature || type == SensorType::Load;
+        static std::wstring str;
+        wchar_t buff[32];
+        swprintf_s(buff, L"%.1f", m_value);
+        str = buff;
+        const auto& settings = CHardwareMonitor::GetInstance()->GetSettings().GetItemInfo(m_identifyer);
+        if (settings.show_unit)
+            str += settings.unit;
+        return str.c_str();
     }
 
-    float CHardwareMonitorItem::GetResourceUsageGraphValue() const
+    int CHardwareMonitorItem::GetItemWidth() const
     {
-        SensorType type = static_cast<SensorType>(sensor_type);
-        if (type == SensorType::Temperature || type == SensorType::Load)
-        {
-            return item_value_num / 100.0f;
-        }
-        return 0.0f;
+        return 0;
     }
 
     void CHardwareMonitorItem::UpdateValue()
     {
-        ISensor^ sensor = HardwareMonitorHelper::FindSensorByIdentifyer(gcnew String(identifier.c_str()));
-        if (sensor != nullptr)
+        ISensor^ sensor = HardwareMonitorHelper::FindSensorByIdentifyer(gcnew String(m_identifyer.c_str()));
+        if (sensor != nullptr && sensor->Value->HasValue)
         {
-            const ItemInfo& item_info{ CHardwareMonitor::GetInstance()->m_settings.FindItemInfo(identifier) };
-            String^ unit;
-            if (!item_info.unit.empty())
-                unit = gcnew String(item_info.unit.c_str());
-            else
-                unit = HardwareMonitorHelper::GetSensorTypeDefaultUnit(sensor->SensorType);
-            const auto& cfg{ CHardwareMonitor::GetInstance()->m_tm_settings };
-            item_value_main_wnd = Common::StringToStdWstring(HardwareMonitorHelper::GetSensorValueText(sensor, unit, item_info.decimal_places, item_info.show_unit, cfg.main_wnd_sperate_with_space));
-            item_value_taskbar = Common::StringToStdWstring(HardwareMonitorHelper::GetSensorValueText(sensor, unit, item_info.decimal_places, item_info.show_unit, cfg.taskbar_sperate_with_space));
-            try
-            {
-                item_value_num = sensor->Value.Value;
-            }
-            catch (System::Exception^)
-            {
-                item_value_num = 0;
-            }
-            sensor_type = static_cast<int>(sensor->SensorType);
+            m_value = sensor->Value->Value;
         }
-    }
-
-    const std::wstring& CHardwareMonitorItem::GetIdentifier() const
-    {
-        return identifier;
     }
 }
